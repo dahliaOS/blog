3c34744cc8ba9033d24d748778378e50fec0b777
SincerelyFaust <hedesmarin@gmail.com>
2022-08-09 16:41:52 +0200

    feat: up nextra to beta.16, remove next-themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,6 @@
-import type { NextPage } from "next";
-import type { AppProps } from "next/app";
-import "nextra-theme-blog/style.css";
-import type { ReactElement, ReactNode } from "react";
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
-export type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-export default function Nextra({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? (page => page);
-
-  return getLayout(<Component {...pageProps} />);
+export default function Blog({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
