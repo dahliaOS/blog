6e34817235d33a689796e217b0eae01d714577a3
SincerelyFaust <hedesmarin@gmail.com>
2022-08-15 19:35:17 +0200

    feat: meta tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -28,18 +28,19 @@ const config = {
   head: () => {
     return (
       <>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="Blog" />
+        <link rel="canonical" href="https://blog.dahliaos.io" />
         <meta name="viewport" content="width=device-width" />
         <meta property="og:image" content="https://imgur.com/pqgjEpd.png" />
-        <meta property="og:site_name" content="dahliaOS Blog" />
-        <meta property="og:title" content="dahliaOS Blog" key="title" />
-        <meta
-          name="description"
-          content="dahliaOS is a modern, secure, lightweight and responsive operating system, combining the best of GNU/Linux and Fuchsia OS."
-        />
-        <meta
-          name="og:description"
-          content="dahliaOS is a modern, secure, lightweight and responsive operating system, combining the best of GNU/Linux and Fuchsia OS."
-        />
+        <meta property="og:site_name" content="dahliaOS" />
+        <meta property="og:title" content="Blog" key="title" />
+        <meta name="description" content="dahliaOS Blog" />
+        <meta name="og:description" content="dahliaOS Blog" />
+        <meta property="og:url" content="https://blog.dahliaos.io"></meta>
+        <meta name="theme-color" content="#ff3d00"></meta>
       </>
     );
   },
