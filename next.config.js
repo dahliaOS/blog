a954d2bc4c9ca8fabbc24cc087016306130f1e68
SincerelyFaust <hedesmarin@gmail.com>
2022-08-30 21:53:18 +0200

    feat: up nextra, add copy code, add reading time

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-blog",
   themeConfig: "./theme.config.tsx",
   unstable_staticImage: true,
+  unstable_defaultShowCopyCode: true,
+  unstable_readingTime: true,
 });
 
 module.exports = withNextra({
